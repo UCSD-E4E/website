78ac95e1a1e62be5e9315c926e439846dcfa30c1
Nathan Hui <ntlhui@gmail.com>
2023-09-01 22:47:52 -0700

    wip: initial files

---
+++ onboarding_papers/create_onboarding_page.py
@@ -0,0 +1,18 @@
+'''Creates the onboarding references page
+'''
+import argparse
+import datetime as dt
+import logging
+import subprocess
+from pathlib import Path
+
+
+def main():
+    """Main application logic
+    """
+    parser = argparse.ArgumentParser()
+    logger = logging.getLogger('create_onboarding_page')
+    
+
+if __name__ == '__main__':
+    main()
